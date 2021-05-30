e07e3561054de2ab5754efbb1b67d81183bd640f
Vinx-exe <84250241+Vinx-exe@users.noreply.github.com>
2021-05-30 06:29:26 +0200

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,10 @@
+do{
+var sign = prompt("Press \"Ok\" to continue to site");
+}while(sign == null || input == "");
+while(sign == "" || input =="");
+// Make sure the word/letter is the same as above
+if (sign.toString() == "Ok") {
+}
+else {
+window.location.reload()
+}
